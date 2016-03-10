c64f14aea6456047d60ad41262363fcd2b9197f7
Eduardo Gochi <gochiberto@gmail.com>
2016-03-09 22:17:34 -0600

    Added BugTable, BugFilter and BugAdd components

--- src/app.js
+++ src/app.js
@@ -6,7 +6,38 @@ var BugList = React.createClass({
   }
 });
 
+var BugTable = React.createClass({
+  render: function() {
+    return (
+      <div>BugTable expected here!</div>
+    )
+  }
+});
+
+var BugFilter = React.createClass({
+  render: function() {
+    return (
+      <div>BugFilter expected here!</div>
+    )
+  }
+});
+
+var BugAdd = React.createClass({
+  render: function() {
+    return (
+      <div>BugAdd expected here!</div>
+    )
+  }
+});
+
 ReactDOM.render(
-    <BugList />,
+	<div>
+	    <h1>Bug Tracker</h1>
+	    <BugFilter />
+	    <hr />
+	    <BugTable />
+	    <hr />
+	    <BugAdd />
+	 </div>,
     document.getElementById('main')
 );
