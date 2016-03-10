109ec85188794090b3b3209a6878d58dab1320d2
Eduardo Gochi <egochi@tiempodevelopment.com>
2016-03-09 21:29:33 -0600

    Added babel-preset-react to manually transform src/App.js to static/App.js (transform.sh script)

--- webapp.js
+++ webapp.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static('static'));
 
-var server = app.listen(3005, function(){
+var server = app.listen(3000, function(){
 	var port = server.address().port;
 	console.log('Started server at port ', port);
 });
