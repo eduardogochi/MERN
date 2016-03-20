e948db309737d0610dc411d828e7b126c1e66003
Eduardo Gochi <gochiberto@gmail.com>
2016-03-19 19:36:18 -0600

    Created initial array of bugs in server and added an endpoint to retrieve them

--- webapp.js
+++ webapp.js
@@ -3,6 +3,31 @@ var app = express();
 
 app.use(express.static('static'));
 
+var bugData = [
+{ id: 1,
+  priority: 'P1',
+  status: 'Open',
+  owner: 'Eduardo',
+  title: 'App crashes on open'
+}, 
+{ id: 2,
+  priority: 'P2',
+  status: 'New',
+  owner: 'Abraham',
+  title: 'Error 404'
+}, 
+{ id: 3,
+  priority: 'P3',
+  status: 'Submitted',
+  owner: 'Oscar',
+  title: 'Server unstable'
+}];
+
+app.get('/api/bugs', function(req,res){
+	res.status(200).send(JSON.stringify(bugData));
+});
+
+
 var server = app.listen(3000, function(){
 	var port = server.address().port;
 	console.log('Started server at port ', port);
