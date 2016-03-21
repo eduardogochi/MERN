8cdfa8c2a126b364e6f66256ff45ed3a3b66def3
Eduardo Gochi <gochiberto@gmail.com>
2016-03-21 01:18:24 -0600

    Change POST API to insert a record into the DB.

--- webapp.js
+++ webapp.js
@@ -7,26 +7,6 @@ var app = express(),
 
 app.use(express.static('static'));
 
-var bugData = [
-{ id: 1,
-  priority: 'P1',
-  status: 'Open',
-  owner: 'Eduardo',
-  title: 'App crashes on open'
-}, 
-{ id: 2,
-  priority: 'P2',
-  status: 'New',
-  owner: 'Lucas',
-  title: 'Error 404'
-}, 
-{ id: 3,
-  priority: 'P3',
-  status: 'Submitted',
-  owner: 'Abraham',
-  title: 'Server unstable'
-}];
-
 app.get('/api/bugs', function(req,res){
 	db.collection("bugs").find().toArray(function(err, docs){
 		res.json(docs)
@@ -36,11 +16,13 @@ app.get('/api/bugs', function(req,res){
 app.use(bodyParser.json());
 app.post('/api/bugs', function(req,res){
 	console.log('Req body: ', req.body);
-
 	var newBug = req.body;
-	newBug.id = bugData.length + 1;
-	bugData.push(newBug);
-	res.json(newBug);
+	db.collection("bugs").insertOne(newBug, function(err, result){
+		var newId = result.insertedId;
+		db.collection("bugs").find({_id: newId}).next(function(err, doc){	
+			res.json(doc);
+			});
+	});
 });
 
 
