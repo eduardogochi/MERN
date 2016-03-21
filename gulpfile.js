cf890ddac346153811f4e597025a8bb8b5acd577
Eduardo Gochi <gochiberto@gmail.com>
2016-03-21 15:55:07 -0600

    Log errors in the source file while watching.

--- gulpfile.js
+++ gulpfile.js
@@ -23,8 +23,13 @@ b.on('update', makeBundle);
 function makeBundle(){
 	b.transform('babelify', {presets:'react'})
 	.bundle()
+	.on('error', function(){
+		console.error(err.message);
+		console.error(err.codeFrame);
+	})
 	.pipe(source('bundle.js'))
 	.pipe(gulp.dest('static/'));
+	console.log("Bundle updated successfully!")
 }
 
 makeBundle();
@@ -32,3 +37,5 @@ makeBundle();
 return b;
 
 });
+
+gulp.task('default', ['watch'])
